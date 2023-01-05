9c334861012253563791b2360480644e3bba5ef5
Admin <preciouspeter3173@gmail.com>
2023-01-05 21:47:29 +0100

    inserting data to the beginning of linked list

--- src/SingleLinked.java
+++ src/SingleLinked.java
@@ -1,16 +1,34 @@
 class SingleLinked{
 
     ListNode head;
-    ListNode node;
+    ListNode newLink;
     int data;
 
 
 // the head is pointed to the data and next to null
+// to add a new link, the next is stored into the firstlink, the first link stores the value back into the link next
 
     public SingleLinked(int data){
 
         this.data = data;
-        node.next = null;
+        head = null;
     }
 
+    public void display(){
+
+        System.out.println(data);
+    }
+
+    public boolean isEmpty(){
+        if (head == null){
+            return false;
+        }
+        return true;
+    }
+    public void insertFirstLink(int data){
+
+      newLink = new ListNode();
+      newLink.next = head;
+      head = newLink;
+    }
 }
